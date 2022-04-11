4ff40b3a7fe846f06dbccdc506759fdd2517d8ae
Bruno Jullien <bruno.jullien@gmail.com>
2022-04-11 16:13:59 -0700

    ESLint and React

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt it!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, {
-        name: "Sudo",
-        animal: "Dog",
-        breed: "Wheaten Terrier",
-      }),
-    ] //array is optional as it would work with multiple params
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
+      <Pet name="Sudo" animal="Dog" breed="">Wheaten Terrier</Pet>
+    </div>
   );
-};
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
